066e4029a67caad6dd2624a94b0ed454c0a7cae8
oxon <oxon@mac.com>
2018-08-29 17:09:25 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+description = '''Common Python toolset for SiPM analysis'''
+
+setup(name='ISEE_SiPM',
+      version='1.0.0',
+      description='Commont Python toolset for SiPM analysis',
+      author='Akira Okumura',
+      author_email='oxon@mac.com',
+      license='MIT License',
+      platforms=['MacOS :: MacOS X', 'POSIX', 'Windows'],
+      url='https://github.com/akira-okumura/ISEE_SiPM',
+      py_modules=['isee_sipm'],
+      install_requires=['numpy'],
+      classifiers=['Topic :: Terminals :: Serial',
+                   'Development Status :: 5 - Production/Stable',
+                   'Programming Language :: Python',
+                   ],
+      long_description=description
+      )
